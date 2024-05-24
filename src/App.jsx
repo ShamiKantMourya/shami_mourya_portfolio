9578e02e229388ca7f2e79719d9bfb2d3d3aa47d
Shami_Mourya <shamimourya9852@gmail.com>
2024-05-24 16:48:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useRoutes } from "react-router-dom";
+
+import "./App.scss";
+import { Home } from "./pages";
+import { Contact } from "./pages";
+import { About } from "./pages";
+import { Portfolio } from "./pages";
+import  Navbar  from "./components";
+
+function App() {
+  let element = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "portfolio",
+      element: <Portfolio />,
+    },
+    {
+      path: "about",
+      element: <About />,
+    },
+    {
+      path: "contact",
+      element: <Contact />,
+    },
+  ]);
+
+  return (
+    <>
+    <Navbar />
+    {element}
+    </>
+  );
+}
+
+export default App;
