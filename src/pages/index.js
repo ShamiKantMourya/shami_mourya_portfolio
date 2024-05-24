87b8ae1eb647a742c057f8c22cf4e660aeaa8855
Shami_Mourya <shamimourya9852@gmail.com>
2024-05-24 16:49:21 +0530

    page

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "./home/Home";
+import About from "./about/About";
+import Contact from "./contact/Contact";
+import Portfolio from "./portfolio/Portfolio";
+
+export { Home, About, Contact, Portfolio };
