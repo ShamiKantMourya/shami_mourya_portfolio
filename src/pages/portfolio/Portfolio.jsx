2753dd7cd97c8a2c74807137b285c3f727bf4278
Shami_Mourya <shamimourya9852@gmail.com>
2024-05-29 17:03:57 +0530

    portfolio page

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+
+import { PortfolioItem } from "../../components";
+import { portfolio } from "../../data";
+import "./portfolio.scss";
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
-  )
-}
+    <section className="portfolio section">
+      <h2 className="section__title">
+        My <span>Portfolio</span>
+      </h2>
+      <div className="portfolio__container container grid">
+        {portfolio?.map((item) => {
+          return <PortfolioItem key={item.id} {...item} />;
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
