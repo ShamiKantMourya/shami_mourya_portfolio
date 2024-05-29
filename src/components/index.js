8a63614c578b8c1ee2b2b1c432f1c603696edb13
Shami_Mourya <shamimourya9852@gmail.com>
2024-05-29 17:03:39 +0530

    portfolio item component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Info from "./info/Info";
 import Stats from "./stats/Stats";
 import Skills from "./skills/Skills";
 import ResumeItem from "./ResumeItem";
+import PortfolioItem from "./PortfolioItem";
 
-export { Navbar, Info, Stats, Skills, ResumeItem };
+export { Navbar, Info, Stats, Skills, ResumeItem, PortfolioItem };
