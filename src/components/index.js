ddd612813fd1536ca6782fc88b982dd333f3ef9b
Shami_Mourya <shamimourya9852@gmail.com>
2024-05-29 12:01:23 +0530

    resume item

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Navbar from "./navbar/Navbar";
 import Info from "./info/Info";
 import Stats from "./stats/Stats";
 import Skills from "./skills/Skills";
+import ResumeItem from "./ResumeItem";
 
-export { Navbar, Info, Stats, Skills };
+export { Navbar, Info, Stats, Skills, ResumeItem };
